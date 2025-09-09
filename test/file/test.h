8700e8488dacf99498601be4d3fa419e0b6b4420
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-09-09 18:12:07 -0300

    Add files via upload

--- test/file/test.h
+++ test/file/test.h
@@ -1,1 +1,23 @@
+// # Copyright (c) 2024 - 2025 Feudal Code Limitada - MIT license #
+
+void testReadTextFile()
+{
+    printf("- testing readTextFile\n"); 
+    
+    String filename = createStringFromLiteral("test.c");        
+    String file = readTextFile(filename);
+
+    String string = createSubstring(&file, 0, 18);
+    String expected = createStringFromLiteral("// # Copyright (c)");
+
+    if (! stringsAreEqual(&expected, &string)) { 
+        printf("readTextFile FAILS!\n"); 
+        exit(1);
+    }
+    
+    releaseHeap(&filename);
+    releaseHeap(&file);
+    releaseHeap(&string);
+    releaseHeap(&expected);
+}
 
