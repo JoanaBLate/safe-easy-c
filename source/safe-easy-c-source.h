ab1bce79f8ab27052f0d25fb9e719966dff7d9af
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-09-09 12:12:45 -0300

    Add files via upload

--- source/safe-easy-c-source.h
+++ source/safe-easy-c-source.h
@@ -1,1 +1,53 @@
+// # Copyright (c) 2024 - 2025 Feudal Code Limitada - MIT license #
+
+#include <math.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "types/types.h"
+
+#include "memory/memory.h"
+
+#include "string/info.h"
+#include "string/create.h"
+#include "string/repeat.h"
+#include "string/append-insert.h"
+#include "string/substring.h"
+#include "string/lower-reverse-sort.h"
+#include "string/pad.h"
+#include "string/print.h"
+#include "string/replace.h"
+#include "string/remove.h"
+#include "string/trim.h"
+#include "string/number.h"
+
+//#include "buffer/info.h"
+//#include "buffer/print.h"
+//#include "buffer/create-from-release.h"
+//#include "buffer/expand.h"
+//#include "buffer/move-range.h"
+//#include "buffer/copy-range.h"
+//#include "buffer/lower-reverse-sort.h"
+//#include "buffer/trim.h"
+//#include "buffer/eat-start-end.h"
+//#include "buffer/replace-start-end.h"
+//#include "buffer/replace-target-all.h"
+//#include "buffer/remove.h"
+//#include "buffer/pad.h"
+//#include "buffer/reset-clear-fill.h"
+
+//#include "array/array.h"
+
+//#include "array-list/array-list.h"
+
+//#include "linked-list/linked-list.h"
+
+//#include "hashmap/hashmap-helper.h"
+//#include "hashmap/hashmap-long.h"
+//#include "hashmap/hashmap-string.h"
+
+#include "file/read.h"
 
