aaa5b762b09a35af1f74a92d108fd21395534012
Joana Borges Late <75029777+JoanaBLate@users.noreply.github.com>
2025-12-07 06:18:27 -0300

    Add files via upload

---
+++ advent-of-code/2025/day03-solve1.c
@@ -0,0 +1,66 @@
+// solution for https://adventofcode.com/2025/day/2 part 1
+
+#include "../safe-easy-c.h"
+
+long processBank(String* bank)
+{
+    int offA = stringSize(bank) - 1;
+    int offB = offA + 1;
+    
+    char bestA = ' ';
+    
+    int home = 0;
+    
+    for (int indexA = 0; indexA < offA; indexA++) 
+    {    
+        char byteA = stringByteAt(bank, indexA);
+        
+        if (byteA > bestA) { bestA = byteA; home = indexA; }
+    }
+    
+    char bestB = ' ';
+    
+    for (int indexB = home + 1; indexB < offB; indexB++) 
+    {        
+        char byteB = stringByteAt(bank, indexB);
+        
+        if (byteB > bestB) { bestB = byteB; }
+    }
+    
+    long a = bestA - '0';
+    long b = bestB - '0';
+    
+    return (10 * a) + b;
+}
+
+int main() 
+{
+    String filename = createStringFromLiteral("day03-input.txt");
+
+    String _puzzleInput = readTextFile(&filename);
+    
+    Buffer puzzleInput = convertStringIntoBuffer(&_puzzleInput); 
+    
+    bufferTrim(&puzzleInput);
+    
+    long result = 0;
+    
+    while (true) 
+    {    
+        String line = bufferEatLine(&puzzleInput);
+        
+        if (stringSize(&line) == 0) { deleteString(&line); break; }
+        
+        result += processBank(&line);
+        
+        deleteString(&line); 
+    }
+
+    printf("answer: %li\n", result);
+
+    deleteString(&filename);
+    deleteBuffer(&puzzleInput);
+
+    return 0;
+}
+
---
+++ advent-of-code/2025/day03-solve2.c
@@ -0,0 +1,79 @@
+// solution for https://adventofcode.com/2025/day/2 part 2
+
+#include "../safe-easy-c.h"
+
+int getBestBatteryIndex(String* bank, int from, int to) 
+{
+    char best = ' ';
+    
+    int position = 0;
+
+    for (int index = from; index <= to; index++) 
+    {    
+        char digit = stringByteAt(bank, index);
+        
+        if (digit > best) { best = digit; position = index; }
+    }
+    
+    return position;
+}
+
+double processBank(String* bank)
+{
+    Buffer buffer = createBuffer(12);
+    
+    int bankSize = (int) stringSize(bank);
+    
+    int maxBankIndex = bankSize - 1 - 12;
+    
+    int bankIndex = -1;
+    
+    for (int bufferIndex = 0; bufferIndex < 12; bufferIndex++)
+    {
+        maxBankIndex += 1;
+        
+        bankIndex = getBestBatteryIndex(bank, bankIndex + 1, maxBankIndex);
+        
+        char byte = stringByteAt(bank, bankIndex);  
+        
+        bufferSetByte(&buffer, bufferIndex, byte);
+    }
+
+    unsigned long number = bufferEatLong(&buffer).value; // assuming number is ok
+    
+    deleteBuffer(&buffer);
+    
+    return number;
+}
+
+int main() 
+{
+    String filename = createStringFromLiteral("day03-input.txt");
+
+    String _puzzleInput = readTextFile(&filename);
+    
+    Buffer puzzleInput = convertStringIntoBuffer(&_puzzleInput); 
+    
+    bufferTrim(&puzzleInput);
+    
+    unsigned long result = 0;
+    
+    while (true) 
+    {    
+        String line = bufferEatLine(&puzzleInput);
+        
+        if (stringSize(&line) == 0) { break; } // need not to be deleted
+        
+        result += processBank(&line);
+        
+        deleteString(&line); 
+    }
+
+    printf("answer: %lu\n", result);
+
+    deleteString(&filename);
+    deleteBuffer(&puzzleInput);
+
+    return 0;
+}
+
